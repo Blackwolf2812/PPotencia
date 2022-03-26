d78e7d7b4484beb7a315e30428da5834e4850f40
Nicolás Olivera <neox2812@gmail.com>
2022-03-26 16:28:17 -0300

    Agregar archivos de proyecto.

---
+++ Matem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPotencia
+{
+    public class Matem
+    {
+        public Matem() { }
+
+        public double Potencia(double pArgumento, int pExponente) 
+        {
+            int exponente = pExponente;
+            double argum = pArgumento;
+            double resultado = pArgumento;
+            if (exponente != 0)
+            {
+                resultado = resultado * Potencia(argum, exponente - 1);
+            }
+            else { return 1; }
+
+            return resultado;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPotencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor usar un b mayor que 0.");
+            Matem matem = new Matem();
+            Console.Write("a: "); 
+            double a= Convert.ToDouble(Console.ReadLine());
+            Console.Write("b: "); 
+            int b= Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("a^b= ");
+            Console.WriteLine(Convert.ToString(matem.Potencia(a,b)));
+            Console.ReadLine();
+        }
+    }
+}
